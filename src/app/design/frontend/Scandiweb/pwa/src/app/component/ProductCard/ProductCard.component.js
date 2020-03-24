608fe4175b0c140d0126f61a5ce03dcebff12ecd
dmitrijs <dmitrijs.voronovs@scandiweb.com>
2020-03-24 16:03:51 +0200

    implemented correct grid

---
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/ProductCard/ProductCard.component.js
@@ -0,0 +1,53 @@
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/base-theme
+ * @link https://github.com/scandipwa/base-theme
+ */
+
+import PropTypes from 'prop-types';
+import SourceProductCard from 'SourceComponent/ProductCard/ProductCard.component';
+import './ProductCard.style';
+
+/**
+ * Product card
+ * @class ProductCard
+ */
+export default class ProductCard extends SourceProductCard {
+    static propTypes = {
+        ...this.propTypes,
+        isHero: PropTypes.bool
+    };
+
+    static defaultProps = {
+        ...this.defaultProps,
+        isHero: false
+    };
+
+    renderAdditionalProductDetails() {
+        const { product: { sku }, getAttribute, isHero } = this.props;
+        const { product_list_content: { attribute_to_display } = {} } = window.contentConfiguration;
+        const brand = getAttribute(attribute_to_display || 'brand') || {};
+
+        if (sku && !brand) return null;
+
+        return (
+            <div
+              block="ProductCard"
+              elem="Brand"
+              mods={ { isLoaded: !!brand } }
+              itemProp="brand"
+            >
+                { /* <ProductAttributeValue
+                  attribute={ brand }
+                  isFormattedAsText
+                /> */ }
+                { isHero ? 'NEW' : '' }
+            </div>
+        );
+    }
+}
