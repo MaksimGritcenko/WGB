bf2f9133adf2a5cd1842ee347bc4ffbc39620709
yeegor <yegor.batov@gmail.com>
2020-03-25 12:20:29 +0200

    Proper slider height for PDP

---
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/Slider/Slider.component.js
@@ -0,0 +1,5 @@
+import SourceSlider from 'SourceComponent/Slider/Slider.component';
+import './Slider.style.override';
+
+export { ANIMATION_DURATION, ACTIVE_SLIDE_PERCENT } from 'SourceComponent/Slider/Slider.component';
+export default class Slider extends SourceSlider {}
