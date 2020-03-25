645acb3a4bb0fb119b037a76255c5d54cda08834
yeegor <yegor.batov@gmail.com>
2020-03-25 16:50:40 +0200

    Give background to related products

---
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/RelatedProducts/RelatedProducts.component.js
@@ -0,0 +1,4 @@
+export { default } from 'SourceComponent/RelatedProducts/RelatedProducts.component';
+export { MAX_NUMBER_OF_PRODUCTS_TO_RENDER } from 'SourceComponent/RelatedProducts/RelatedProducts.component';
+
+import './RelatedProducts.override.style.scss';
