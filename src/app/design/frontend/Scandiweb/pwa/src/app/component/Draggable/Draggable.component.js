e51da8ebac6db327247765b1f1ddd6ca8aaafa11
yeegor <yegor.batov@gmail.com>
2020-03-26 20:22:34 +0200

    Not prevent default on mobile to be able to select Remove redundant styling file

--- src/app/design/frontend/Scandiweb/pwa/src/app/component/Draggable/Draggable.component.js
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/Draggable/Draggable.component.js
@@ -1,2 +1,16 @@
-export { default } from 'SourceComponent/Draggable/Draggable.component';
-import './Draggable.override.style.scss';
+import SourceDraggable from 'SourceComponent/Draggable/Draggable.component';
+import isMobile from 'Util/Mobile';
+
+class Draggable extends SourceDraggable {
+    handleMouseDown = (event) => {
+        window.addEventListener('mousemove', this.handleMouseMove);
+        window.addEventListener('mouseup', this.handleMouseUp);
+
+        if (!isMobile.any()) {
+            event.preventDefault();
+        }
+        this._handleDragStart(event);
+    };
+}
+
+export default Draggable;
