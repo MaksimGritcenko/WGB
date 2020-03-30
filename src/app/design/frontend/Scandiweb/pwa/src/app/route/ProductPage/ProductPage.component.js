0b5dd315efee652081558c149629d482bfffdc23
yeegor <yegor.batov@gmail.com>
2020-03-30 15:10:42 +0300

    Fix linter issues

--- src/app/design/frontend/Scandiweb/pwa/src/app/route/ProductPage/ProductPage.component.js
+++ src/app/design/frontend/Scandiweb/pwa/src/app/route/ProductPage/ProductPage.component.js
@@ -18,48 +18,50 @@ export default class ProductPage extends SourceProductPage {
     }
 
     renderGoBackIcon() {
-        return <>
+        return (
             <svg
-                block="ProductPage"
-                elem="GoBackIcon"
-                width="19px" height="18px" viewBox="0 0 19 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink"
+              block="ProductPage"
+              elem="GoBackIcon"
+              width="19px" height="18px" viewBox="0 0 19 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink"
             >
                 <g id="Page-2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                     <g id="PDP_mobile" transform="translate(-15.000000, -66.000000)" stroke="#090403">
                         <g id="Group-3" transform="translate(16.000000, 66.000000)">
                             <g id="Group-2">
-                                <polyline id="Path-2" transform="translate(4.482767, 9.241384) scale(1, -1) rotate(90.000000) translate(-4.482767, -9.241384) " points="-4 5 4.48276724 13.4827672 12.9655345 5"></polyline>
-                                <polyline id="Path-3" points="0.241383619 9.24138362 10.2205848 9.24138362 17.3483583 9.24138362"></polyline>
+                                <polyline id="Path-2" transform="translate(4.482767, 9.241384) scale(1, -1) rotate(90.000000) translate(-4.482767, -9.241384) " points="-4 5 4.48276724 13.4827672 12.9655345 5"/>
+                                <polyline id="Path-3" points="0.241383619 9.24138362 10.2205848 9.24138362 17.3483583 9.24138362"/>
                             </g>
                         </g>
                     </g>
                 </g>
             </svg>
-        </>
+        );
     }
 
     renderGoBackText(categoryName) {
-        return <>
+        return (
+        <>
             <p
               block="ProductPage"
               elem="GoBackText"
             >
-                {__('Back to ')}
+                { __('Back to ') }
                 <span
                   block="ProductPage"
                   elem="Category"
                 >
-                    {categoryName}
+                    { categoryName }
                 </span>
             </p>
         </>
+        );
     }
 
     renderPDPHeader() {
         const { name: categoryName, url_path } = this.getCategory() || '';
 
         if (!url_path) return null;
-        return <>
+        return (
             <div block="ProductPage" elem="Header">
                 <Link
                   block="ProductPage"
@@ -70,24 +72,26 @@ export default class ProductPage extends SourceProductPage {
                     { this.renderGoBackText(categoryName) }
                 </Link>
             </div>
-        </>
+        );
     }
 
     renderProductGallery() {
         const { productOrVariant, areDetailsLoaded } = this.props;
         return (
             <ProductGallery
-              product={productOrVariant}
-              areDetailsLoaded={areDetailsLoaded}
+              product={ productOrVariant }
+              areDetailsLoaded={ areDetailsLoaded }
             />
-        )
+        );
     }
 
     renderProductPageContent() {
-        return <>
+        return (
+        <>
             { this.renderPDPHeader() }
             { this.renderProductGallery() }
         </>
+        );
     }
 
     freezeScroll() {
@@ -120,25 +124,25 @@ export default class ProductPage extends SourceProductPage {
         return (
             <DragBar>
                 <ProductActions
-                    getLink={getLink}
-                    updateConfigurableVariant={updateConfigurableVariant}
-                    product={dataSource}
-                    productOrVariant={productOrVariant}
-                    parameters={parameters}
-                    areDetailsLoaded={areDetailsLoaded}
-                    configurableVariantIndex={configurableVariantIndex}
+                  getLink={ getLink }
+                  updateConfigurableVariant={ updateConfigurableVariant }
+                  product={ dataSource }
+                  productOrVariant={ productOrVariant }
+                  parameters={ parameters }
+                  areDetailsLoaded={ areDetailsLoaded }
+                  configurableVariantIndex={ configurableVariantIndex }
                 />
                 <ProductInformation
-                    product={{ ...dataSource, parameters }}
-                    areDetailsLoaded={areDetailsLoaded}
+                  product={ { ...dataSource, parameters } }
+                  areDetailsLoaded={ areDetailsLoaded }
                 />
                 <ProductReviews
-                    product={dataSource}
-                    areDetailsLoaded={areDetailsLoaded}
+                  product={ dataSource }
+                  areDetailsLoaded={ areDetailsLoaded }
                 />
                 <RelatedProducts
-                    product={dataSource}
-                    areDetailsLoaded={areDetailsLoaded}
+                  product={ dataSource }
+                  areDetailsLoaded={ areDetailsLoaded }
                 />
             </DragBar>
         );
