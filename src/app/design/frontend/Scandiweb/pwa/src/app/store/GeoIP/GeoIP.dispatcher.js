16b1f1970aa761fc43ec00fc5b7c789ce9a175cd
dmitrijs <dmitrijs.voronovs@scandiweb.com>
2020-04-14 17:32:24 +0300

    fix deplot

--- src/app/design/frontend/Scandiweb/pwa/src/app/store/GeoIP/GeoIP.dispatcher.js
+++ src/app/design/frontend/Scandiweb/pwa/src/app/store/GeoIP/GeoIP.dispatcher.js
@@ -3,7 +3,7 @@ import { showNotification } from 'Store/Notification';
 import { QueryDispatcher } from 'Util/Request';
 import GeoIPQuery from 'Query/GeoIP.query';
 import { updateGeolocation } from 'Store/GeoIP';
-import publicIp from 'public-ip';
+// import publicIp from 'public-ip';
 
 export class GeoIPDispatcher extends QueryDispatcher {
     constructor() {
@@ -21,12 +21,12 @@ export class GeoIPDispatcher extends QueryDispatcher {
 
     async handleData(dispatch, options) {
         dispatch(updateGeolocation({ isLoading: true }));
-        try {
-            this.userIp = await publicIp.v4();
-        } catch (error) {
-            dispatch(updateGeolocation({ isLoading: false, error: true }));
-            return;
-        }
+        // try {
+        //     this.userIp = await publicIp.v4();
+        // } catch (error) {
+        //     dispatch(updateGeolocation({ isLoading: false, error: true }));
+        //     return;
+        // }
         super.handleData(dispatch, options);
     }
 
