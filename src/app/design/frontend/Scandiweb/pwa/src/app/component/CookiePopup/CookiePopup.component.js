68016afd40047a3d79bb059a2ae2b9819eea705f
dmitrijs <dmitrijs.voronovs@scandiweb.com>
2020-04-15 15:45:22 +0300

    fix cookie text and link

--- src/app/design/frontend/Scandiweb/pwa/src/app/component/CookiePopup/CookiePopup.component.js
+++ src/app/design/frontend/Scandiweb/pwa/src/app/component/CookiePopup/CookiePopup.component.js
@@ -60,8 +60,6 @@ export class CookiePopup extends SourceCookiePopup {
               elem="Link"
               to={ absoluteCookieLink }
             >
-                <br />
-                { __('Click here for more information on our ') }
                 <u>{ __('Cookies policy.') }</u>
             </Link>
         );
@@ -111,6 +109,8 @@ export class CookiePopup extends SourceCookiePopup {
         return (
         <p>
             { cookieText }
+            <br />
+            { __('Click here for more information on our ') }
             { this.renderCookieLink() }
         </p>
         );
