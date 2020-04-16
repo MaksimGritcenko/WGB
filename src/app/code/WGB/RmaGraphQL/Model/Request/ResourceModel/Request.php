88303765144db8940a180271d0163108ad59dfaa
yeegor <yegor.batov@gmail.com>
2020-04-16 13:20:19 +0300

    Add item qty retrieval to request list query

--- src/app/code/WGB/RmaGraphQL/Model/Request/ResourceModel/Request.php
+++ src/app/code/WGB/RmaGraphQL/Model/Request/ResourceModel/Request.php
@@ -3,6 +3,7 @@
 namespace WGB\RmaGraphQL\Model\Request\ResourceModel;
 
 use Amasty\Rma\Api\Data\RequestInterface;
+use Amasty\Rma\Model\Request\ResourceModel\RequestItem;
 use Magento\Framework\DB\Select;
 
 class Request extends \Amasty\Rma\Model\Request\ResourceModel\Request {
@@ -14,7 +15,21 @@ class Request extends \Amasty\Rma\Model\Request\ResourceModel\Request {
     public function getRequestsForUser($userId) {
         // TODO columns
         $select = $this->getConnection()->select()->from(['request' => $this->getMainTable()])
-            ->where('request.'.RequestInterface::CUSTOMER_ID.' = '.$userId);
+            ->reset(Select::COLUMNS)
+            ->columns([
+                'request.order_id',
+                'request.request_id',
+                'request.created_at',
+                'request.status',
+                'request_qty' => 'sum(items.request_qty)'
+            ])
+            ->where('request.'.RequestInterface::CUSTOMER_ID.' = '.$userId)
+            ->joinInner(
+                ['items' => $this->getTable(RequestItem::TABLE_NAME)],
+                'request.request_id = items.request_id',
+                []
+            )
+            ->group('request.request_id');
 
         $result = [];
         if ($rows = $this->getConnection()->fetchAll($select)) {
