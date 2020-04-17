1af784018a8b19b63448fd9ebc3662d567210a87
yeegor <yegor.batov@gmail.com>
2020-04-17 15:24:27 +0300

    Remove redundant member

--- src/app/code/WGB/RmaGraphQL/Model/Resolver/AddTrackingToRequest.php
+++ src/app/code/WGB/RmaGraphQL/Model/Resolver/AddTrackingToRequest.php
@@ -36,10 +36,6 @@ class AddTrackingToRequest implements ResolverInterface
      * @var Session
      */
     protected $customerSession;
-    /**
-     * @var ResourceModel\Request
-     */
-    protected $requestResourceModel;
 
     public function __construct(
         Session $customerSession,
