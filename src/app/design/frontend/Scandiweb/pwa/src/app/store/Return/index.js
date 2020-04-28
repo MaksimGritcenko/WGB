41a5aae00f86ff104fa9ab1295a609619e3398a1
yeegor <yegor.batov@gmail.com>
2020-04-28 17:01:26 +0300

    Latest chat functionality: sending messages

--- src/app/design/frontend/Scandiweb/pwa/src/app/store/Return/index.js
+++ src/app/design/frontend/Scandiweb/pwa/src/app/store/Return/index.js
@@ -1,5 +1,3 @@
 export { default as ReturnReducer } from './Return.reducer';
-export {
-    default as ReturnDispatcher
-} from './Return.dispatcher';
+export { default as ReturnDispatcher } from './Return.dispatcher';
 export * from './Return.action';
