1fa6fb4c15aee85135f942cf42f2bc1d371dbbfb
dmitrijs <dmitrijs.voronovs@scandiweb.com>
2020-04-28 13:36:43 +0300

    no more mailchimp

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -224,7 +224,6 @@ return [
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
         'Dotdigitalgroup_Email' => 1,
-        'Ebizmarts_MailChimp' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
